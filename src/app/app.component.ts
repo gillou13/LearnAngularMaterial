21731dc061732429bdb438991f1826c01cabcd01
Gilles Biguet <gilles.biguet@gmail.com>
2024-04-18 00:22:42 +0200

    headbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeadbarComponent } from './headbar/headbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeadbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
---
+++ src/app/headbar/headbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';  
+
+@Component({
+  selector: 'app-headbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,],
+  templateUrl: './headbar.component.html',
+  styleUrl: './headbar.component.sass'
+})
+export class HeadbarComponent {
+
+}
