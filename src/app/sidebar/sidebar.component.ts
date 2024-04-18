1a956fc09f83f36f12c848463e247e780f8f935b
Gilles Biguet <gilles.biguet@gmail.com>
2024-04-18 23:29:05 +0200

    sidbar2

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatSidenavModule, MatListModule, RouterLink, CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.sass'
+})
+export class SidebarComponent {
+  public links = [
+    { routerLink: "/test1", label: "test1"},
+    { routerLink: "/tab1", label: "tab1"},
+  ]
+}
