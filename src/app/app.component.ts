05f1cd3b609f7cdca25ef417d2595c08864ceed3
Gilles Biguet <gilles.biguet@gmail.com>
2024-04-18 23:28:50 +0200

    sidnav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadbarComponent } from './headbar/headbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeadbarComponent],
+  imports: [RouterOutlet, HeadbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Test1Component } from './test1/test1.component';
+import { Tab1Component } from './tab1/tab1.component';
 
 export const routes: Routes = [
     {path: 'test1', component: Test1Component},
+    {path: 'tab1', component: Tab1Component},
     {path: '', redirectTo: '/test1', pathMatch: 'full'}
 ];
