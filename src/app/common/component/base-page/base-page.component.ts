5e5240e52822f327bfe33a0b80f92b66db738880
Gilles Biguet <gilles.biguet@gmail.com>
2024-09-11 02:01:57 +0200

    base-page simplification

--- src/app/common/component/base-page/base-page.component.ts
+++ src/app/common/component/base-page/base-page.component.ts
@@ -5,32 +5,30 @@ import { Subscription } from 'rxjs';
 
 /**
  * Base de composant à destination d'une page.
- * La fonction createLink doit être définit pour que la page fonctionne.
+ * Avec
+ * - la gestion de la navigation par onglet.
+ * (La fonction createLink doit être définit pour que la page fonctionne)
+ * - la liste des subscriptions (pour evité de refaire le job de partout)
  */
 @Component({
   selector: 'app-base-page',
   standalone: true,
-  imports: [],
-  templateUrl: './base-page.component.html',
-  styleUrl: './base-page.component.sass',
+  template: '<p>exemple<p>',
 })
 export abstract class BasePageComponent implements OnDestroy {
-  /** le lien courrant est soit récupéré via le service de navigation, soit crée par le composant.
-   *
+  /**
+   * Le lien courrant est soit récupéré via le service de navigation, soit crée par le composant.
    * Il est possible de la modifier par la suite.
    */
   public currentLink: NavigationLink;
 
   /** Service de navigation */
-  protected navigationService: NavigationService;
+  protected navigationService = inject(NavigationService);
 
   /** Tableau de subscriptions pour la gestion des observables dans les composants. */
   protected subscriptions: Subscription[] = new Array<Subscription>();
 
   constructor() {
-    // injection du service de navigation.
-    this.navigationService = inject(NavigationService);
-
     // Init du lien courrant.
     this.currentLink = this.navigationService.setNewLink(this.createLink);
   }
